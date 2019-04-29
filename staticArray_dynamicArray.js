4335963bcb33a125bc28af6a581bc993167fc1e7
fuelerAirman <40779030+92fueler@users.noreply.github.com>
2019-04-29 15:46:52 -0700

    Create staticArray_dynamicArray.js

---
+++ staticArray_dynamicArray.js
@@ -0,0 +1,19 @@
+//static array vs dynamic array
+
+/*
+dynamic array 
+lookup 
+append 
+insert 
+delete 
+*/
+
+//c++ --> you have to assign memory ahead of time. 
+int a[10];
+int b[5] {1, 2, 3, 4, 5}
+
+
+/*
+In JS and other languages like Python where you have lists and array list in Java
+they work like dynamic arrays. They automatically allocate memory according to the increase in size of the array. 
+*/
