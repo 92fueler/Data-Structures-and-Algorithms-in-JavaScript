342374b0ba772dc84d56f38fc096ba7a740dc1e7
fuelerAirman <40779030+92fueler@users.noreply.github.com>
2019-04-28 22:45:36 -0700

    Create hashTable.js

---
+++ hashTable.js
@@ -0,0 +1,21 @@
+//hash table or hash maps or ordered maps or dictionaries 
+
+
+/*
+every language has its built-in hash table just like array
+in Python it's called dictionaries, in JS we call objects 
+In ruby, it's called hash. 
+
+Where this name comes from? 
+
+Hash tables:
+we have a key, and we send it through a hash function that is going to hash something really really fast 
+and then map whatever the hash comes out into a memory address where we can store the data 
+
+
+hash function is a function that generates a value ramdom pattern 
+example: md5 hash generator, sha-1 hash generator, sha-256 hash generator 
+
+idemponent 
+
+*/
