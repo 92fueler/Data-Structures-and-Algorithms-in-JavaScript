b400358e9e724c50304610a9026583ef73031c18
fuelerAirman <40779030+92fueler@users.noreply.github.com>
2019-04-29 15:35:12 -0700

    Create dataStructure.js

---
+++ dataStructure.js
@@ -0,0 +1,27 @@
+//data structures + algorithms = programs 
+
+/*
+data structure is a collection of values. The values can have relationships among them and 
+they can have functions applied to them. 
+readable 
+memory: space complexity  
+speed: time complexity 
+1. how to build one 
+2. how to use it 
+How computers store data? 
+CPU, RAM, Storage 
+Data structures: 
+arrays 
+stacks 
+queues 
+linked l ists 
+trees 
+tries 
+graphs 
+hash tables 
+Algorithms:
+sorting
+dynamic programming 
+BFS + DFS (searching)
+recursion 
+*/
