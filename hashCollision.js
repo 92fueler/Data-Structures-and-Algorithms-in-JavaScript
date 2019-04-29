cc7bb3222b81f59197d948e4fbe6eb14e52a0b71
fuelerAirman <40779030+92fueler@users.noreply.github.com>
2019-04-29 16:03:27 -0700

    Update hashCollision.js

--- hashCollision.js
+++ hashCollision.js
@@ -7,6 +7,8 @@ delete  o(1)
 search  o(1)
 */
 
+//hash table in JS is an object. 
+
 let user = {
   age: 54,
   name: 'Kylie',
@@ -15,8 +17,21 @@ let user = {
     console.log('ahaha');
   }
 }
-usesr.age;//o(1)
-user.spell = 'abra kada';//o(1)
+usesr.age;//access --> o(1)
+user.spell = 'abra kada';//insert --> o(1)
 user.scream();//o(1)
 
-//hash collision
+//What's the main problem using hash table? --> hash collision
+/*
+while we create the hash table, the memory assign the space for us, not the all the space the memory has 
+but just a little bit of it. 
+hash collision means assigning keys into the same address since assigning is random-based. 
+with enough data and limited memory we cannot avoid the collision. 
+when we have collision, it slows reading and writing. 
+
+There are different ways to solve the collision.
+
+
+*/
+
+
