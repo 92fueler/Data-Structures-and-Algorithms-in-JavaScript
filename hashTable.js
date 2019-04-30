94a1d4b4a07ed8ed2072d10007a48b0310908868
fuelerAirman <40779030+92fueler@users.noreply.github.com>
2019-04-29 19:29:51 -0700

    Update hashTable.js

--- hashTable.js
+++ hashTable.js
@@ -1,10 +1,11 @@
 //hash table or hash maps or ordered maps or dictionaries 
 
+//hash table is a data structure which organizes data using hash functions in order to support quick insertion and search. 
+
 
 /*
-every language has its built-in hash table just like array
-in Python it's called dictionaries, in JS we call objects 
-In ruby, it's called hash. 
+every language has its built-in hash table just like array,
+in Python it's called dictionaries; in JS we call objects; In ruby, it's called hash. 
 
 Where this name comes from? 
 
@@ -12,8 +13,10 @@ Hash tables:
 we have a key, and we send it through a hash function that is going to hash something really really fast 
 and then map whatever the hash comes out into a memory address where we can store the data 
 
-
-hash function is a function that generates a value ramdom pattern 
+Hash function:
+it maps the key to a specific bucket. 
+example: 
+we use y = x % 5 as a hash function, where x is the key value and y is the index of the assigned bucket. 
 example: md5 hash generator, sha-1 hash generator, sha-256 hash generator 
 
 idemponent 
