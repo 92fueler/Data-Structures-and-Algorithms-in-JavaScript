3839354ce7a2f0b4bbd70023df39e928d06c2c93
fuelerAirman <40779030+92fueler@users.noreply.github.com>
2019-04-30 15:45:54 -0700

    Create threeStacksInOneArray.js

---
+++ threeStacksInOneArray.js
@@ -0,0 +1,21 @@
+//implement 3 stacks with 1 array in such a way that neither stack overflows unless the total number of elements 
+//in both stacks together is n. 
+//The push() and pop() methods should run in O(1) time. 
+
+/*
+properties:
+_stackCapacity
+values： an 
+sizes 
+numberOfStacks: 
+
+methods: 
+get stackCapacity()
+push(stackNumber, value)
+pop(stackNumber)
+peek(stackNumber)
+isEmpty(stackNumber)
+isFull(stackNumber)
+indexOfTop(stackNumber) --> a helper method which returns the index, in the values array, of the top element in the 
+respective stack. 
+*/
