bf5a54f07918c6adcae010260dbf97543507308e
fuelerAirman <40779030+92fueler@users.noreply.github.com>
2019-04-30 15:47:26 -0700

    Create twoStacksInOneArray.js

---
+++ twoStacksInOneArray.js
@@ -0,0 +1,4 @@
+//implement two stacks in one array A[1...n] in such a way that neither stack overflows 
+//unless the total number of elements in both stacks together in n. 
+//The PUSH and POP operations should run in O(1) time. 
+
