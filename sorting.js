ba43657325a6f7193fec667d656ab67a3ff4477d
fuelerAirman <40779030+92fueler@users.noreply.github.com>
2019-05-01 19:03:17 -0700

    Create sorting.js

---
+++ sorting.js
@@ -0,0 +1,18 @@
+//sorting
+
+/*
+sorting:
+1. insertion sort
+2. merger 
+solving recurrence 
+*/
+
+/*
+binary serach: 
+A[0:n] --> looking for a specific item 
+
+not obvious sorting problem:
+data compression
+other sorting problems for example: cocktail sort, bitonic sort 
+*/
+
