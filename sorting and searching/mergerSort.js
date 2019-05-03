8fe60dc8ac8c7411112f2a29bb083fe97c69d8d3
fuelerAirman <40779030+92fueler@users.noreply.github.com>
2019-05-03 01:39:57 -0700

    Create mergerSort.js

---
+++ sorting and searching/mergerSort.js
@@ -0,0 +1,13 @@
+//merger sort 
+
+/*
+MERGE-SORT A[1, ..., n]
+  if n = 1, done (nothing to sort)
+  otherwise, recursively sort A[1, ..., n/2] and A[n/2 + 1, ..., n]
+  merge the two sorted sub-arrays 
+*/
+
+//time complexity 
+/*
+Time = theta(n) to merge a total of n elements (linear time) 
+*/
