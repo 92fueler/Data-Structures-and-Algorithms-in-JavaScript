bfcb3abb812b16becd4d5b327f72e224da953e34
fuelerAirman <40779030+92fueler@users.noreply.github.com>
2019-05-02 23:12:11 -0700

    Update insertionSort.js

--- insertionSort.js
+++ insertionSort.js
@@ -1,5 +1,34 @@
 //insertion sort 
 
+//How to think of this algorithm?
+/*
+The way it works is that it splits the array into two sections - a sorted and an unsorted one. 
+And, we start our sorted section at the first item --> an array of a single item is sorted for sure. 
+
+insertion means insert one item from the unsorted section into the sorted section. 
+*/
+
+/*
+given a sorted array arr = [1, 2, 5, 9] and a value val = 4
+write a program to insert the value into the sorted array.
+*/
+
+let arr = [1, 2, 5, 9];
+const val = 4;
+
+function soring(arr, val){
+  let newArr = arr.push(val); 
+  for(let i = newArr.length - 2; i >= 0; i--){
+    if(newArr[i] > val){
+      newArr[i + 1] = arr[i];
+      newArr[i] = val;
+    }
+  }
+  return newArr;
+}
+
+
+
 /*
 INSERTION-SORT(A)
 for j = 2 to A.length
@@ -25,3 +54,4 @@ function insertionSort(arr){
 }
 
 const arr = [11, 3, 45, 67, 2, 1, 40, 90, 23];
+
