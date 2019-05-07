f6fe42536332c1058fedfb5685df5bdb632f7e13
fuelerAirman <40779030+92fueler@users.noreply.github.com>
2019-05-06 23:05:43 -0700

    Update inversion.js

--- sorting and searching/inversion.js
+++ sorting and searching/inversion.js
@@ -1,5 +1,11 @@
 //source: CLRS Chapter 2 problem: 2 -4 
 /*
 Let A[1, ..., n] be an array of n distinct numbers. 
-If i < j 
+If i < j and A[i] > A[j], then the pair (i, j) is called an inversion of A.
+a. list the five inservions of the array <2, 3, 8, 6, 1>
+b. what array with elements from the set {1, 2, ..., n} has the most inversions? How many does it have?
+c. what is the relationship between the running time of insertion sort and the number of inversions in the input 
+   array? Justify your answer.
+d. Give an algorithm that determines the number of inversions in any permutation on n elements in theta(nlgn) 
+   worst-case time. (Hint: modify merge sort.) 
 */
