2b461dc6980cdbd246f721417e59a890cf3c11d4
Jian Wu <40779030+92fueler@users.noreply.github.com>
2019-10-27 13:45:30 -0700

    Update staticArrayVSdynamicArray.js

--- array and string/staticArrayVSdynamicArray.js
+++ array and string/staticArrayVSdynamicArray.js
@@ -10,10 +10,10 @@ if we want more memory.
 
 /*
 dynamic array 
-lookup 
-append 
-insert 
-delete 
+lookup o(1)
+append o(1) --> can be o(n) --> loop over the array and copy those elements to a new place and append one element to it
+insert o(n)
+delete o(n)
 */
 
 //c++ --> you have to assign memory ahead of time. 
@@ -22,6 +22,6 @@ int b[5] {1, 2, 3, 4, 5}
 
 
 /*
-In JS and other languages like Python where you have lists and array list in Java
-they work like dynamic arrays. They automatically allocate memory according to the increase in size of the array. 
+In JS and other languages like Python where you have lists and array list in Java,they work like dynamic arrays. 
+They automatically allocate memory according to the increase in size of the array. 
 */
