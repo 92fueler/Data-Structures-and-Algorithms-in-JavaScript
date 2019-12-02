6e89a62c73bccd63667d88ce2e482fb3a5a2cba3
Jian Wu <40779030+92fueler@users.noreply.github.com>
2019-12-02 14:24:20 -0800

    Create hash table.js

---
+++ hash table/hash table.js
@@ -0,0 +1,32 @@
+//object in JavaScript is hash table
+
+
+/*
+hash function 
+idempotent 
+
+key-value --> can be any types in JS 
+key can be functions, numbers and so on too. 
+In JS, it will be strintified to the strings stored in the object.
+
+However, ES6, we have Map and Set 
+const a = new Map()
+const b = new Sets() 
+
+
+
+
+lookup --> o(1) 
+insert --> o(1)
+delete --> o(1)
+search --> o(1) 
+
+cons:
+hash collision 
+--> limited space and lots of data --> cannot avoid collision 
+
+two common ways: 
+1. linked list 
+2. 
+*/
+
