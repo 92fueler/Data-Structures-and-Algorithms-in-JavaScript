d9bb1958674ae71e88d0a924619b0dceb49e04d2
Jian Wu <40779030+92fueler@users.noreply.github.com>
2020-01-08 17:18:06 -0800

    Create binary search tree.js

---
+++ tree/binary search tree.js
@@ -0,0 +1,15 @@
+/*
+binary search tree:
+--> this structure preserves the relationships --> the relationship of parent and child
+
+lookup: O(logN)
+insert: O(logN)
+delete: O(logN)
+
+*/
+
+/*
+balanced vs. unbalanced tree
+
+u
+*/
