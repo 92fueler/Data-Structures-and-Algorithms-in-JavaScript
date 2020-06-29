065c8e4e891f8a8bdd465f76806704409a951943
Jian Wu <cocopig@Jians-MacBook-Air.local>
2020-06-28 19:11:26 -0700

    stack updates

--- 02_stack/stack.js
+++ 02_stack/stack.js
@@ -1,39 +1,49 @@
-
 class Stack {
     constructor() {
-        //property;
-        this.items = [];
+        /*
+        Here it's bettern to set items as the private variable;
+        otherwise, the items property of an instance can be edited by 
+        directly assining the value to stack.items, rather than using 
+        the methods to alter the items. 
+        */
+        // this.items = [];
+        var items = [];
+
         //methods:
         //push 
         this.push = function (element) {
-            this.items.push(element);
+            items.push(element); // return is not needed here
         };
         //pop 
         this.pop = function () {
-            return this.items.pop();
+            return items.pop();
         };
         //peek --> return the top element 
         this.peek = function () {
-            return this.items[this.items.length - 1];
+            return items[items.length - 1];
         };
         //isEmpty 
         this.isEmpty = function () {
-            return this.items.length === 0;
+            return items.length === 0;
         };
         //size 
         this.size = function () {
-            return this.items.length;
+            return items.length;
         };
         //clear --> remove all the elements 
         this.clear = function () {
-            return this.items = [];
+            return items = [];
         };
         this.toString = function () {
-            var stringStack = '';
-            for (var i = 0; i < this.items.length; i++) {
-                stringStack += items[i].toString;
-            }
-            return stringStack;
-        };
+            if (this.isEmpty === true) {
+                return 'this is an empty stack'
+            } else {
+                var stringStack = '';
+                for (var i = 0; i < items.length; i++) {
+                    stringStack += items[i].toString() + ' ';
+                }
+                return stringStack;
+            };
+        }
     }
 }
